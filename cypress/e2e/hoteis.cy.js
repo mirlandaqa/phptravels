da2586b0df5329cd836d39aa35bad18298c20baf
Mirlanda Costa <mirlanda.dsc@gmail.com>
2022-11-17 23:17:06 -0400

    Adicionando pesquisa de hotel

--- cypress/e2e/hoteis.cy.js
+++ cypress/e2e/hoteis.cy.js
@@ -4,25 +4,28 @@ describe('Buscar hoteis para viagem', () => {
         })
 
 
-        it('Buscar  cidade', () =>{
+        it.only('Pesquisar hoteis', () =>{
 
-            cy.contains('Hotels').should('be.visible')
+            //Selecionar cidade
             cy.get('[aria-labelledby="select2-hotels_city-container"]').click({force:true})
             cy.get('.select2-search__field').type('Capri')
-            cy.scrollTo('top')
+            cy.scrollTo('top') //Voltar página para o topo
+            cy.wait(14000)
             cy.contains('Capri,Italy').click()
             cy.scrollTo('top')
             cy.get('span').contains('Capri,Italy').should('be.visible')
-        })
 
-        it.only('Buscar data de checkin', () =>{
-
-            cy.contains('Hotels').should('be.visible')
-            cy.scrollTo('top')
-
+            //Selecionar data de checkin
             cy.get('input[id="checkin"]').click({force:true})
             cy.get('tr:nth-child(4) > td:nth-child(6)').contains('25').click()
-
             cy.scrollTo('top')
+  
+            //Selecionar data de checkout
+            cy.get('tr:nth-child(4) > td.day.active').contains('26').click()
+            cy.scrollTo('top')
+
+            //Buscar dados
+            cy.get('#hotels-search > .main_search > div.g-1 > .col-md-1 > .btn-search > #submit').click({force:true})
         })
-})
+
+    })
