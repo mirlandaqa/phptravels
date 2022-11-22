c399338e5ba5c49b0b87f3e8f778ed35927cde6b
Mirlanda Costa <mirlanda.dsc@gmail.com>
2022-11-22 02:07:22 -0400

    Adicionar click na aba Visto

---
+++ cypress/e2e/visto.cy.js
@@ -0,0 +1,13 @@
+describe('Buscar visto', () => {
+
+    beforeEach(()=>{
+        cy.visit('https://phptravels.net/')
+
+        cy.get('button[id="flights-tab"]').click({force:true})
+    })
+
+    it('Buscar visto', ()=>{
+        cy.get('#visa-tab').click({force:true})
+        
+    })
+})
